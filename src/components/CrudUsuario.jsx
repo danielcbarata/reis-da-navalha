3e4838e55c2ac598f44be69aa2b6a5761db1ef48
Daniel <dcbarata@unifei.edu.br>
2023-11-27 18:48:35 -0300

    Iniciando a página

---
+++ src/components/CrudUsuario.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function CrudUsuario() {
+    return (
+        <div id="page">
+            <div id="header">
+                <p>Reis da Navalha</p>
+            </div>
+            <div id="menu">
+                <p>Sobre Nós</p>
+            </div>
+            <div id="menu-sobre">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, nam omnis. Distinctio velit aliquam amet quae, quod facilis labore blanditiis voluptates? Eligendi excepturi dignissimos pariatur rerum, fugit quasi expedita cum.</p>
+            </div>
+            <div id="body">
+                <p>Realizar Agendamento</p>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './components/CrudUsuario.css';
 import reportWebVitals from './reportWebVitals';
+import CrudUsuario from './components/CrudUsuario';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CrudUsuario />
   </React.StrictMode>
 );
 
