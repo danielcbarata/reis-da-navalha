18d89293eca145680d899188839a65e2ebc3123b
Thocantunes <thomaz_campos@outlook.com>
2023-12-03 19:29:02 -0300

    Menu suspenso
    Criação do menu suspenso e corpo do css

--- src/components/CrudUsuario.jsx
+++ src/components/CrudUsuario.jsx
@@ -3,10 +3,7 @@ import Modal from 'react-modal';
 
 export default function CrudUsuarios() {
     const [usuarios, setUsuarios] = useState([]);
-    const [id_usuario, setId_Usuario] = useState("");
-    const [nome, setNome] = useState("");
-    const [telefone, setTelefone] = useState("");
-    const [horarios, setHorarios] = useState("");
+    const [showMenu, setShowMenu] = useState(false); // Definindo o estado para controlar a visibilidade do menu
 
     const url = "https://agenda-omega-liart.vercel.app/usuarios/";
 
@@ -19,29 +16,34 @@ export default function CrudUsuarios() {
 
     return (
         <div id="page">
-            <div id="header">
-                <h1 class="titulo">Reis da Navalha</h1>
-            </div>
-            <div id="menu">
-                <p>Sobre Nós</p>
-            </div>
-            <div id="menu-sobre">
-                {usuarios ? usuarios.map((item) => {
-                    return (
-                        <div key={item.id_usuario}>
-                            {item.id_usuario} - {item.nome} - {item.telefone} - {item.horarios}
-                        </div>
-                    );
-                })
-                    : false}
-            </div>
-            <div id="body">
-                <p>Realizar Agendamento</p>
-            </div>
-            <Modal aria-labelledby="modal-title" aria-describedby="modal-description">
-                <h2 id="modal-title">My Title</h2>
-                <p id="modal-description">My Description</p>
-            </Modal>
+        <div id="header">
+            <h1 className="titulo">Reis da Navalha</h1>
         </div>
-    )
+        <div 
+            id="menu" 
+            className={`menu-bar ${showMenu ? 'show-menu' : ''}`} 
+            onMouseEnter={() => setShowMenu(true)} 
+            onMouseLeave={() => setShowMenu(false)}
+        >
+            <p>Sobre Nós</p>
+        </div>
+        <div id="menu-sobre" className="menu-sobre">
+            {/* Conteúdo do menu suspenso */}
+            {usuarios ? usuarios.map((item) => {
+                return (
+                    <div key={item.id_usuario}>
+                        {item.id_usuario} - {item.nome} - {item.telefone} - {item.horarios}
+                    </div>
+                );
+            }) : null}
+        </div>
+        <div id="body">
+            <p>Realizar Agendamento</p>
+        </div>
+        <Modal aria-labelledby="modal-title" aria-describedby="modal-description">
+            <h2 id="modal-title">My Title</h2>
+            <p id="modal-description">My Description</p>
+        </Modal>
+    </div>
+)
 }
